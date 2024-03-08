20bacb6fe02d75b96e230620a94a470c25c3dc92
jayanthzarco <126959745+jayanthzarco@users.noreply.github.com>
2024-03-08 13:12:17 +0530

    Update one

---
+++ Follicle_on_selected_position.py
@@ -0,0 +1,11 @@
+# this script helps to create a follicle on selected position
+# select any vertices or uv and run the script
+# code is created on 20-05-2021
+# published 08-March-2024 by Jayanth
+
+
+
+
+
+
+"""created by Jayanth"""
