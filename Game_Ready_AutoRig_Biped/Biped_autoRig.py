4db56f6d07b45a22a3c35d3a9fbad55ccea2d390
jayanthzarco <126959745+jayanthzarco@users.noreply.github.com>
2024-10-25 19:53:31 +0530

    completed build skeleton

--- Game_Ready_AutoRig_Biped/Biped_autoRig.py
+++ Game_Ready_AutoRig_Biped/Biped_autoRig.py
@@ -2,13 +2,14 @@ import maya.cmds as cmd
 from PySide2 import QtWidgets
 
 
-class Node_Names:
+class NodeNames:
     def __init__(self):
         self.placement_joints = {
             'root': 'Root',
             'spine_01': 'Spine_01',
             'spine_02': 'Spine_02',
             'spine_03': 'Spine_03',
+            'spine_04': 'Spine_04',
             'LT_Clavicle': 'LT_Clavicle',
             'LT_Shoulder': 'LT_Shoulder',
             'LT_Elbow': 'LT_Elbow',
@@ -32,8 +33,22 @@ class Node_Names:
             'root_ctrl': 'Root_Ctrl'
         }
 
+        self.skin_jnts = {key: value + "_Skn_jnt" for key, value in self.placement_joints.items()}
+        self.asset_names = {
+            'asset': 'Asset',
+            'geo_grp': 'Geometry_Group',
+            'rig_grp': 'Rig_Group',
+            'extras': 'Extras',
+            'skn_jnts_grp': 'Skn_Jnts_Group',
+            'ctrls_group': 'Ctrl_Group',
+            'placement_jnts_grp': 'Placements_Jnts_Group'
+        }
+
 
 class InfoMessageBox:
+    def __init__(self):
+        self.message = None
+
     def show(self, message):
         # Create a message box
         self.message = message
@@ -73,6 +88,11 @@ class Window:
 
     def build_skeleton(self, *args):
         print("Build Skeleton action triggered")
+        check = self.loc_jnt.error_check()
+        if check:
+            return
+        self.loc_jnt.create_placement_joints()
+        self.loc_jnt.build_asset()
         self.loc_jnt.mirror_jnts()
 
     def generate_rig(self, *args):
@@ -80,7 +100,7 @@ class Window:
         GenerateRig()
 
 
-class LocationJoint(Node_Names):
+class LocationJoint(NodeNames):
     def __init__(self):
         super().__init__()
         self.msg_ui = InfoMessageBox()
@@ -94,19 +114,23 @@ class LocationJoint(Node_Names):
         print(error_obj)
         if error_obj:
             self.msg_ui.show(", ".join(error_obj) + " already exists in the scene remove it before creating Joints")
+            return
         root_ctrl = cmds.circle(name=self.placement_joints['root_ctrl'], center=(0, 0, 0), radius=4, normal=(0, 1, 0),
                                 ch=False)
         cmds.select(clear=True)
 
         # root jnt
-        root_joint = cmds.joint(name=self.placement_joints['root'], position=(0, 5, 0))
-        cmds.joint(name=self.placement_joints['spine_01'], position=(0, 6, 0))
-        cmds.joint(name=self.placement_joints['spine_02'], position=(0, 7, 0))
-        cmds.joint(name=self.placement_joints['spine_03'], position=(0, 8, 0))
+        root_joint = cmds.joint(name=self.placement_joints['root'], position=(0, 4.5, 0))
+        cmds.select(cl=True)
+        spine_joint = cmds.joint(name=self.placement_joints['spine_01'], position=(0, 5, 0))
+        cmds.joint(name=self.placement_joints['spine_02'], position=(0, 6, 0))
+        cmds.joint(name=self.placement_joints['spine_03'], position=(0, 7, 0))
+        cmds.joint(name=self.placement_joints['spine_04'], position=(0, 8, 0))
 
         # Reorient spine joints
         cmds.joint(root_joint, edit=True, orientJoint='xyz', secondaryAxisOrient='yup', ch=False, zso=True)
         cmds.parent(root_joint, root_ctrl)
+        cmds.parent(spine_joint, root_ctrl)
 
         # head and neck
         cmds.select(clear=True)
@@ -150,20 +174,105 @@ class LocationJoint(Node_Names):
         self.msg_ui.show("Placement joints are created")
 
     def mirror_jnts(self):
-        cmds.mirrorJoint("LT_Thigh", mirrorYZ=True, mirrorBehavior=True, searchReplace=('LT_', 'RT_'))
-        cmds.mirrorJoint("LT_Clavicle", mirrorYZ=True, mirrorBehavior=True, searchReplace=('LT_', 'RT_'))
+        cmds.mirrorJoint(self.skin_jnts["LT_Thigh"], mirrorYZ=True, mirrorBehavior=True, searchReplace=('LT_', 'RT_'))
+        cmds.mirrorJoint(self.skin_jnts["LT_Clavicle"], mirrorYZ=True, mirrorBehavior=True,
+                         searchReplace=('LT_', 'RT_'))
         self.msg_ui.show("Skeleton built check the orientation of joints")
 
+    def create_placement_joints(self):
+        # creating arms and legs skin joints
+        cmds.select(cl=True)
+        clv_jnt = cmds.joint(name=self.skin_jnts['LT_Clavicle'])
+        shl_jnt = cmds.joint(name=self.skin_jnts['LT_Shoulder'])
+        elbow_jnt = cmds.joint(name=self.skin_jnts['LT_Elbow'])
+        wrist_jnt = cmds.joint(name=self.skin_jnts['LT_Wrist'])
+        cmds.matchTransform(clv_jnt, self.placement_joints['LT_Clavicle'])
+        cmds.matchTransform(shl_jnt, self.placement_joints['LT_Shoulder'])
+        cmds.matchTransform(elbow_jnt, self.placement_joints['LT_Elbow'])
+        cmds.matchTransform(wrist_jnt, self.placement_joints['LT_Wrist'])
 
-class GenerateRig:
+        clv_jnt_grp = cmds.group(clv_jnt, name=clv_jnt + "_Group")
+
+        # creating leg
+        cmds.select(cl=True)
+        thigh_jnt = cmds.joint(name=self.skin_jnts['LT_Thigh'])
+        keen_jnt = cmds.joint(name=self.skin_jnts['LT_Knee'])
+        ankle_jnt = cmds.joint(name=self.skin_jnts['LT_Ankle'])
+        ball_jnt = cmds.joint(name=self.skin_jnts['LT_Ball'])
+        toe_jnt = cmds.joint(name=self.skin_jnts['LT_Toe'])
+        cmds.matchTransform(thigh_jnt, self.placement_joints['LT_Thigh'])
+        cmds.matchTransform(keen_jnt, self.placement_joints['LT_Knee'])
+        cmds.matchTransform(ankle_jnt, self.placement_joints['LT_Ankle'])
+        cmds.matchTransform(ball_jnt, self.placement_joints['LT_Ball'])
+        cmds.matchTransform(toe_jnt, self.placement_joints['LT_Toe'])
+
+        thigh_jnt_grp = cmds.group(thigh_jnt, name=thigh_jnt + '_Group')
+
+        # creating spine and head
+        cmds.select(cl=True)
+        root_jnt = cmds.joint(name=self.skin_jnts['root'])
+        cmds.select(cl=True)
+        spine_01 = cmds.joint(name=self.skin_jnts['spine_01'])
+        spine_02 = cmds.joint(name=self.skin_jnts['spine_02'])
+        spine_03 = cmds.joint(name=self.skin_jnts['spine_03'])
+        spine_04 = cmds.joint(name=self.skin_jnts['spine_04'])
+
+        cmds.matchTransform(root_jnt, self.placement_joints['root'])
+        cmds.matchTransform(spine_01, self.placement_joints['spine_01'])
+        cmds.matchTransform(spine_02, self.placement_joints['spine_02'])
+        cmds.matchTransform(spine_03, self.placement_joints['spine_03'])
+        cmds.matchTransform(spine_04, self.placement_joints['spine_04'])
+
+        spine_jnt_group = cmds.group(spine_01, name='Spine_Skn_Jnt_Group')
+
+        cmds.select(cl=True)
+        neck_jnt = cmds.joint(name=self.skin_jnts['Neck'])
+        head_jnt = cmds.joint(name=self.skin_jnts['Head'])
+        cmds.matchTransform(neck_jnt, self.placement_joints['Neck'])
+        cmds.matchTransform(head_jnt, self.placement_joints['Head'])
+
+        head_jnt_group = cmds.group(neck_jnt, name='Head_Skn_Jnts_Group')
+        cmds.group(root_jnt, head_jnt_group, spine_jnt_group, clv_jnt_grp,
+                   thigh_jnt_grp, name=self.asset_names['skn_jnts_grp'])
+
+    def error_check(self):
+        error_obj = []
+        for item in self.asset_names.values():
+            if cmds.objExists(item):
+                error_obj.append(item)
+        if error_obj:
+            self.msg_ui.show(", ".join(error_obj)+" already exists in the scene")
+            return True
+
+    def build_asset(self):
+        cmds.select(cl=True)
+        asset = cmds.group(name=self.asset_names['asset'], em=True)
+        geo = cmds.group(name=self.asset_names['geo_grp'], em=True)
+        rig = cmds.group(name=self.asset_names['rig_grp'], em=True)
+        extras = cmds.group(name=self.asset_names['extras'], em=True)
+        ctrls_group = cmds.group(name=self.asset_names['ctrls_group'], em=True)
+        placement_jnts_grp = cmds.group(name=self.asset_names['placement_jnts_grp'], em=True)
+        cmds.parent(ctrls_group, rig)
+        cmds.parent(geo, asset)
+        cmds.parent(rig, asset)
+        cmds.parent(extras, asset)
+        cmds.parent(self.asset_names['skn_jnts_grp'], rig)
+        cmds.parent(self.placement_joints['root_ctrl'], placement_jnts_grp)
+        cmds.parent(self.asset_names['placement_jnts_grp'], extras)
+
+        cmds.setAttr(extras + ".visibility", 0)
+        cmds.setAttr(extras + ".visibility", lock=True, keyable=False, channelBox=False)
+
+
+class GenerateRig(NodeNames):
     def __init__(self):
-        self.names = Node_Names()
-        self.placement_joints = self.names.placement_joints
-        print(self.placement_joints)
+        super().__init__()
         self.build_rig()
 
     def build_rig(self):
-        cmds.group(self.placement_joints['root_ctrl'], name="asset")
+        # creating globe ctrls and checking asset
+        pass
+
 
 
 if __name__ == "__main__":
