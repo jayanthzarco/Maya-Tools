c2de26e4737d0665ca9f22dfeeba282c56a1eeee
jayanthzarco <126959745+jayanthzarco@users.noreply.github.com>
2024-10-26 23:22:35 +0530

    completed build skeleton

--- Game_Ready_AutoRig_Biped/Biped_autoRig.py
+++ Game_Ready_AutoRig_Biped/Biped_autoRig.py
@@ -33,7 +33,7 @@ class NodeNames:
             'root_ctrl': 'Root_Ctrl'
         }
 
-        self.skin_jnts = {key: value + "_Skn_jnt" for key, value in self.placement_joints.items()}
+        self.skin_jnts = {key: value + "_Skn_Jnt" for key, value in self.placement_joints.items()}
         self.asset_names = {
             'asset': 'Asset',
             'geo_grp': 'Geometry_Group',
@@ -44,6 +44,33 @@ class NodeNames:
             'placement_jnts_grp': 'Placements_Jnts_Group',
             'placement_ctrl': 'Placement_Ctrl',
             'main_ctrl': 'Main_Ctrl'
+        }
+
+        self.ctrls = {
+            'cog_ctrl': 'COG_Ctrl',
+            'hip_ctrl': 'Hip_Ctrl',
+            'spine_IK_01': 'Spine_IK_01_Ctrl',
+            'spine_IK_02': 'Spine_IK_02_Ctrl',
+            'spine_IK_03': 'Spine_IK_03_Ctrl',
+
+            'spine_FK_01': 'Spine_FK_01_Ctrl',
+            'spine_FK_02': 'Spine_FK_02_Ctrl',
+            'spine_FK_03': 'Spine_FK_03_Ctrl',
+
+            'neck': 'Neck_Ctrl',
+            'LT_Arm_IK': "LT_Arm_IK_Ctrl",
+            'LT_Arm_Pole': "LT_Arm_Pole_Ctrl",
+            'RT_Arm_IK': "RT_Arm_IK_Ctrl",
+            'RT_Arm_Pole': "LT_Arm_Pole_Ctrl",
+
+            "LT_Leg_IK": "LT_Leg_IK_Ctrl",
+            "RT_Leg_IK": "RT_Leg_IK_Ctrl",
+            "LT_Leg_Pole": "LT_Leg_Pole_Ctrl",
+            "RT_Leg_Pole": "RT_Leg_Pole_Ctrl",
+
+            'LT_Clavicle': "LT_Clavicle_Ctrl",
+            'RT_Clavicle': "RT_Clavicle_Ctrl"
+
         }
 
 
@@ -270,29 +297,134 @@ class GenerateRig(NodeNames):
     def __init__(self):
         super().__init__()
         self.ctrls_shapes = ControlShapes()
+        self.custom_rig = Rig_Customs()
         self.build_rig()
 
     def build_rig(self):
         # creating globe ctrls and checking asset
-        main_ctrl = self.ctrls_shapes.circle(name=self.asset_names['main_ctrl'], radius=3, color=13)
+        main_ctrl = self.ctrls_shapes.circle(name=self.asset_names['main_ctrl'], radius=3, color=6)
 
-        plc_ctrl = self.ctrls_shapes.circle(name=self.asset_names['placement_ctrl'], radius=2.5, color=6)
+        plc_ctrl = self.ctrls_shapes.cog(name=self.asset_names['placement_ctrl'], color=13)
         plc_grp = cmds.group(plc_ctrl, name=plc_ctrl + "_Group")
         cmds.parent(main_ctrl, self.asset_names['ctrls_group'])
         cmds.parent(plc_grp, main_ctrl)
 
+        # COG and spine
+        cog_ctrl = self.ctrls_shapes.cog(name="COG_Ctrl", color=13)
+        cog_group = cmds.group(cog_ctrl, name=cog_ctrl + "_Group")
+        cmds.parent(cog_group, plc_ctrl)
+        cmds.matchTransform(cog_group, self.skin_jnts['spine_01'])
 
-        # cube
-        cube = self.ctrls_shapes.cube(color=13, name="IK_CTRL")
-        plus = self.ctrls_shapes.plus()
+        # hip ctrl
+        hip_ctrl = self.ctrls_shapes.circle(name="Hip_Ctrl", radius=2.2, color=6)
+        hip_group = cmds.group(hip_ctrl, name=hip_ctrl + "_Group")
+        hip_offset = cmds.group(hip_group, name=hip_ctrl + "_Offset_Group")
+        cmds.matchTransform(hip_offset, self.skin_jnts['root'])
+        cmds.parent(hip_offset, cog_ctrl)
+
+        # spine ik handel
+        spine_ik_handle, effector, spine_curve = cmds.ikHandle(
+            sj=self.skin_jnts['spine_01'],
+            ee=self.skin_jnts['spine_04'],
+            solver="ikSplineSolver",
+            createCurve=True,
+            simplifyCurve=False
+        )
+        cmds.rebuildCurve(spine_curve, degree=1, keepRange=1)
+        spine_def = cmds.group(spine_ik_handle, spine_curve, name="spine_ik_handel_group")
+        cmds.parent(spine_def, self.asset_names['extras'])
+
+        # create a bind joints for ik
+        spine_bind_jnts = self.custom_rig.joint_along_the_curve(no_of_joints=3, curve=spine_curve)
+        spine_bind_grp = cmds.group(spine_bind_jnts[0], name="spine_bind_joints_group")
+        cmds.parent(spine_bind_grp, self.asset_names['extras'])
+        cmds.skinCluster(spine_bind_jnts, spine_curve, tsb=True)
+
+        # create spine ik ctrls
+        spine_01_ik = self.ctrls_shapes.cube(color=13, name="Spine_01_IK_Ctrl")
+        spine_02_ik = self.ctrls_shapes.cube(color=13, name="Spine_02_IK_Ctrl")
+        spine_03_ik = self.ctrls_shapes.cube(color=13, name="Spine_03_IK_Ctrl")
+
+        spine_01_ik_offset = cmds.group(spine_01_ik, name=spine_01_ik + "_Offset_Group")
+        spine_02_ik_offset = cmds.group(spine_02_ik, name=spine_02_ik + "_Offset_Group")
+        spine_03_ik_offset = cmds.group(spine_03_ik, name=spine_03_ik + "_Offset_Group")
+
+        cmds.matchTransform(spine_01_ik_offset, spine_bind_jnts[0])
+        cmds.matchTransform(spine_02_ik_offset, spine_bind_jnts[1])
+        cmds.matchTransform(spine_03_ik_offset, spine_bind_jnts[2])
+
+        cmds.parentConstraint(spine_01_ik, spine_bind_jnts[0], mo=True)
+        cmds.parentConstraint(spine_02_ik, spine_bind_jnts[1], mo=True)
+        cmds.parentConstraint(spine_03_ik, spine_bind_jnts[2], mo=True)
+
+        cmds.parent(spine_01_ik_offset, hip_ctrl)
+
+        # creating fk ctrls for spine
+        spine_01_fk = self.ctrls_shapes.circle(name="Spine_01_FK_Ctrl", radius=1.5, color=18)
+        spine_02_fk = self.ctrls_shapes.circle(name="Spine_02_FK_Ctrl", radius=1.5, color=18)
+        spine_03_fk = self.ctrls_shapes.circle(name="Spine_03_FK_Ctrl", radius=1.5, color=18)
+        spine_01_fk_offset = cmds.group(spine_01_fk, name=spine_01_fk + "_Offset_Group")
+        spine_02_fk_offset = cmds.group(spine_02_fk, name=spine_02_fk + "_Offset_Group")
+        spine_03_fk_offset = cmds.group(spine_03_fk, name=spine_03_fk + "_Offset_Group")
+
+        cmds.matchTransform(spine_01_fk_offset, spine_01_ik_offset)
+        cmds.matchTransform(spine_02_fk_offset, spine_02_ik_offset)
+        cmds.matchTransform(spine_03_fk_offset, spine_03_ik_offset)
+
+        cmds.parent(spine_01_fk_offset, cog_ctrl)
+        cmds.parent(spine_02_fk_offset, spine_01_fk)
+        cmds.parent(spine_03_fk_offset, spine_02_fk)
+
+        cmds.parent(spine_02_ik_offset, spine_02_fk)
+        cmds.parent(spine_03_ik_offset, spine_03_fk)
+        cmds.scaleConstraint(cog_ctrl, self.asset_names["skn_jnts_grp"], mo=True)
+
+        # twist for spine
+        spine_pma = cmds.createNode("plusMinusAverage", name="Spine_Twist_PMA")
+        cmds.connectAttr('Spine_01_FK_Ctrl.rotateY', spine_pma+'.input1D[0]')
+        cmds.connectAttr('Spine_02_FK_Ctrl.rotateY', spine_pma+'.input1D[1]')
+        cmds.connectAttr('Spine_03_FK_Ctrl.rotateY', spine_pma+'.input1D[2]')
+        cmds.connectAttr(spine_pma+'.output1D', spine_ik_handle+".twist")
+
+        # creating neck ctrl
+        neck_ctrl = self.ctrls_shapes.circle(name="Neck_Ctrl", radius=1, color=18)
+        neck_offset_grp = cmds.group(neck_ctrl, name=neck_ctrl + "_Offset_Group")
+        cmds.matchTransform(neck_offset_grp, self.skin_jnts["Neck"])
+        cmds.parent(neck_offset_grp, cog_ctrl)
+
+        cmds.parentConstraint(neck_ctrl, self.skin_jnts['Neck'], mo=True)
+        cmds.scaleConstraint(neck_ctrl, self.skin_jnts['Neck'], mo=True)
+        cmds.parentConstraint(self.skin_jnts['spine_04'], neck_offset_grp, mo=True)
+
+        # create clv ctrl
+        lt_clv_ctrl = self.ctrls_shapes.circle(name=self.ctrls['LT_Clavicle'], color=18)
+        rt_clv_ctrl = self.ctrls_shapes.circle(name=self.ctrls['RT_Clavicle'], color=18)
+
+        lt_clv_offset = cmds.group(lt_clv_ctrl, name=lt_clv_ctrl+"_Offset_group")
+        rt_clv_offset = cmds.group(rt_clv_ctrl, name=rt_clv_ctrl+"_Offset_group")
+        cmds.matchTransform(lt_clv_offset, self.skin_jnts['LT_Clavicle'])
+        cmds.matchTransform(rt_clv_offset, self.skin_jnts['RT_Clavicle'])
+        cmds.parent(lt_clv_offset, cog_ctrl)
+        cmds.parent(rt_clv_offset, cog_ctrl)
+
+        cmds.parentConstraint(lt_clv_ctrl, self.skin_jnts['LT_Clavicle'], mo=True)
+        cmds.parentConstraint(rt_clv_ctrl, self.skin_jnts['RT_Clavicle'], mo=True)
+        cmds.parentConstraint(self.skin_jnts['spine_04'], lt_clv_offset, mo=True)
+        cmds.parentConstraint(self.skin_jnts['spine_04'], rt_clv_offset, mo=True)
+
+        # limb rig
+        self.custom_rig.limb_rig(side='LT', shape="Arm")
+        self.custom_rig.limb_rig(side='RT', shape="Arm")
+        self.custom_rig.limb_rig(side='LT', shape="leg")
+        self.custom_rig.limb_rig(side='RT', shape="Leg")
 
 
 class ControlShapes:
     @staticmethod
     def circle(name='circle', radius=1.0, color=1):
-        circle = cmds.circle(name=name, radius=radius,center=(0, 0, 0), normal=(0, 1, 0), ch=0)[0]
+        circle = cmds.circle(name=name, radius=radius, center=(0, 0, 0), normal=(0, 1, 0), ch=0)[0]
         shape_node = cmds.listRelatives(circle, shapes=True)[0]
-        cmds.setAttr(shape_node+".overrideEnabled", 1)
+        cmds.setAttr(shape_node + ".overrideEnabled", 1)
         cmds.setAttr(shape_node + ".overrideColor", color)
         return circle
 
@@ -317,7 +449,7 @@ class ControlShapes:
         curve = cmds.curve(name=name, degree=1, point=points)
 
         shape_node = cmds.listRelatives(curve, shapes=True)[0]
-        cmds.setAttr(shape_node+".overrideEnabled", 1)
+        cmds.setAttr(shape_node + ".overrideEnabled", 1)
         cmds.setAttr(shape_node + ".overrideColor", color)
         return curve
 
@@ -348,9 +480,215 @@ class ControlShapes:
         # Create the curve with degree 1
         curve_ = cmds.curve(name=name, degree=1, point=points)
         shape_node = cmds.listRelatives(curve_, shapes=True)[0]
-        cmds.setAttr(shape_node+".overrideEnabled", 1)
+        cmds.setAttr(shape_node + ".overrideEnabled", 1)
         cmds.setAttr(shape_node + ".overrideColor", color)
         return curve_
+
+    @staticmethod
+    def cog(name='COG_Ctrl', color=14):
+        curve = cmds.curve(name=name,
+                           d=1,
+                           p=[(-2.121687, 0, 0),
+                              (-1.491384, 0, -0.679417),
+                              (-1.491384, 0, -0.357191),
+                              (-1.09932, 0, -0.357191),
+                              (-0.935137, 0, -0.679417),
+                              (-0.679417, 0, -0.935137),
+                              (-0.357191, 0, -1.09932),
+                              (-0.357191, 0, -1.35728),
+                              (-0.671052, 0, -1.35728),
+                              (0, 0, -2),
+                              (0.679417, 0, -1.35728),
+                              (0.357191, 0, -1.35728),
+                              (0.357191, 0, -1.09932),
+                              (0.679417, 0, -0.935138),
+                              (0.935138, 0, -0.679417),
+                              (1.09932, 0, -0.357191),
+                              (1.454919, 0, -0.357191),
+                              (1.454919, 0, -0.679417),
+                              (2.1117, 0, 0.001089),
+                              (1.454919, 0, 0.671052),
+                              (1.454919, 0, 0.357191),
+                              (1.09932, 0, 0.357191),
+                              (0.935137, 0, 0.679417),
+                              (0.679417, 0, 0.935137),
+                              (0.357191, 0, 1.09932),
+                              (0.349338, 0, 1.323075),
+                              (0.679417, 0, 1.323075),
+                              (0, 0, 2),
+                              (-0.671052, 0, 1.323075),
+                              (-0.357733, 0, 1.323075),
+                              (-0.357191, 0, 1.09932),
+                              (-0.679417, 0, 0.935137),
+                              (-0.935137, 0, 0.679417),
+                              (-1.09932, 0, 0.357191),
+                              (-1.491385, 0, 0.352793),
+                              (-1.491385, 0, 0.671052),
+                              (-2.121687, 0, 0)
+                              ],
+                           k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                              21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+                           )
+        shape_node = cmds.listRelatives(curve, shapes=True)[0]
+        cmds.setAttr(shape_node + ".overrideEnabled", 1)
+        cmds.setAttr(shape_node + ".overrideColor", color)
+        return curve
+
+    @staticmethod
+    def square(name="Square", color=14):
+        curve = cmds.curve(name=name, d=1, p=[(-1, 0, -1), (1, 0, -1), (1, 0, 1), (-1, 0, 1), (-1, 0, -1)],
+                           k=[0, 1, 2, 3, 4])
+        shape_node = cmds.listRelatives(curve, shapes=True)[0]
+        cmds.setAttr(shape_node + ".overrideEnabled", 1)
+        cmds.setAttr(shape_node + ".overrideColor", color)
+        return curve
+
+    @staticmethod
+    def loc(name="loc", color=12):
+        curve = cmds.curve(name=name, d=1, p=[(0, 0, 0), (0, 0, -1), (0, 0, 0), (1, 0, 0), (0, 0, 0), (0, 0, 1),
+                                              (0, 0, 0), (-1, 0, 0), (0, 0, 0), (0, 1, 0), (0, 0, 0), (0, -1, 0)],
+                           k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+        shape_node = cmds.listRelatives(curve, shapes=True)[0]
+        cmds.setAttr(shape_node + ".overrideEnabled", 1)
+        cmds.setAttr(shape_node + ".overrideColor", color)
+        return curve
+
+
+class Rig_Customs(NodeNames):
+    def __init__(self):
+        super().__init__()
+        self.ctrl_shapes = ControlShapes()
+
+    @staticmethod
+    def joint_along_the_curve(no_of_joints, curve):
+        selcurve = curve
+        No_Of_Jnt = no_of_joints
+        bindJnt = []
+        newJnt = []
+        prevesJnt = ''
+        rootJnt = ''
+        for jnt in range(0, No_Of_Jnt):
+            cmds.select(cl=True)
+            newjnt = cmds.joint()
+            bindJnt.append(newjnt)
+            motionpath = cmds.pathAnimation(newjnt, c=selcurve, fm=True)
+            cmds.cutKey(motionpath + '.u', t=())
+            cmds.setAttr(motionpath + '.u', jnt * (1.0 / (No_Of_Jnt - 1)))
+            cmds.delete(newjnt + '.tx', icn=True)
+            cmds.delete(newjnt + '.ty', icn=True)
+            cmds.delete(newjnt + '.tz', icn=True)
+            cmds.delete(motionpath)
+            if jnt == 0:
+                prevesJnt = newjnt
+                rootJnt = newjnt
+                continue
+            cmds.parent(newjnt, prevesJnt)
+            prevesJnt = newjnt
+        cmds.joint(rootJnt, e=True, oj='xyz', sao='yup', ch=True, zso=True)
+        for i, x in enumerate(bindJnt):
+            newname = selcurve + '_' + str(i + 1).zfill(2)
+            newJnt.append(newname + '_Jnt')
+        for a, b in zip(bindJnt, newJnt):
+            cmds.rename(a, b)
+        return newJnt
+
+    def limb_rig(self, side="LT", shape="Arm"):
+        if shape == "Arm":
+            parent, ctrl = "Clavicle_Skn_Jnt", side+"_Clavicle_Ctrl"
+        else:
+            parent, ctrl = "Root_Skn_Jnt", "Hip_Ctrl"
+
+        if shape == "Arm":
+            base_jnt = "_Shoulder_Skn_Jnt"
+            mid_jnt = "_Elbow_Skn_Jnt"
+            end_jnt = "_Wrist_Skn_Jnt"
+        else:
+            base_jnt = "_Thigh_Skn_Jnt"
+            mid_jnt = "_Knee_Skn_Jnt"
+            end_jnt = "_Ankle_Skn_Jnt"
+
+        # IK CHAIN
+        cmds.select(cl=True)
+        ik_01 = cmds.joint(name=side + base_jnt.replace("Skn", "IK"))
+        ik_02 = cmds.joint(name=side + mid_jnt.replace("Skn", "IK"))
+        ik_03 = cmds.joint(name=side + end_jnt.replace("Skn", "IK"))
+
+        cmds.matchTransform(ik_01, side + base_jnt)
+        cmds.matchTransform(ik_02, side + mid_jnt)
+        cmds.matchTransform(ik_03, side + end_jnt)
+        cmds.select(cl=True)
+        cmds.makeIdentity(ik_01, apply=True, translate=True, rotate=True, scale=True, normal=False, preserveNormals=True)
+
+        # creating_ctrls and matching positions
+        ik_jnt_grp = cmds.group(ik_01, name=side + "_" + shape + "_IK_Jnts")
+        ik_ctrl = self.ctrl_shapes.square(name=side + "_" + shape + "_IK_Ctrl", color=13)
+        pole_ctrl = self.ctrl_shapes.loc(name=side + "_" + shape + "_Pole_Ctrl", color=13)
+        ik_offset = cmds.group(ik_ctrl, name=ik_ctrl + "_Offset_Group")
+        pole_offset = cmds.group(pole_ctrl, name=pole_ctrl + "_Offset_Group")
+        cmds.matchTransform(ik_offset, ik_03)
+        cmds.matchTransform(pole_offset, ik_02)
+
+        ik_grp = cmds.group([ik_offset, pole_offset], name=side + "_" + shape + "_IK_Ctrls_Group")
+        ik_handle = cmds.ikHandle(
+            name=side+"_"+shape+"_IK_handel",
+            startJoint=ik_01,
+            endEffector=ik_03,
+            solver="ikRPsolver"
+
+        )
+        cmds.parentConstraint(ik_ctrl, ik_handle[0], mo=True)
+        cmds.poleVectorConstraint(pole_ctrl, ik_handle[0])
+        def_grp = cmds.group(ik_jnt_grp, name=ik_jnt_grp.replace("IK", "DEF"))
+        cmds.parent(ik_grp, "COG_Ctrl")
+        cmds.parentConstraint(self.asset_names['placement_ctrl'], ik_jnt_grp, mo=True)
+        cmds.scaleConstraint(self.asset_names['placement_ctrl'], ik_jnt_grp, mo=True)
+        cmds.parentConstraint(ctrl, ik_01, mo=True)
+        ik_def = cmds.group([ik_handle[0], ik_jnt_grp], name=side+'_'+shape+"_ik_def_group")
+        cmds.parent(ik_def, def_grp)
+
+        # FK Chain
+        cmds.select(cl=True)
+        fk_01 = cmds.joint(name=side + base_jnt.replace("Skn", "FK"))
+        fk_02 = cmds.joint(name=side + mid_jnt.replace("Skn", "FK"))
+        fk_03 = cmds.joint(name=side + end_jnt.replace("Skn", "FK"))
+
+        cmds.matchTransform(fk_01, side + base_jnt)
+        cmds.matchTransform(fk_02, side + mid_jnt)
+        cmds.matchTransform(fk_03, side + end_jnt)
+        cmds.select(cl=True)
+        cmds.makeIdentity(fk_01, apply=True, translate=True, rotate=True, scale=True, normal=False, preserveNormals=True)
+        # creating fk ctrls
+        for each in [fk_01, fk_02, fk_03]:
+            print(each)
+            fk_ctrl = self.ctrl_shapes.circle(each.replace('Jnt', 'Ctrl'), radius=0.5, color=18)
+            ctrl_group = cmds.group(fk_ctrl, name=fk_ctrl + "_Group")
+            offset = cmds.group(ctrl_group, name=ctrl_group.replace("_Group", "_Offset_Group"))
+            cmds.matchTransform(offset, each)
+            cmds.parentConstraint(fk_ctrl, each, mo=True)
+
+        cmds.parent(fk_03.replace("Jnt", 'Ctrl_Offset_Group'), fk_02.replace("Jnt", 'Ctrl'))
+        cmds.parent(fk_02.replace("Jnt", 'Ctrl_Offset_Group'), fk_01.replace("Jnt", 'Ctrl'))
+
+        cmds.parent(fk_01.replace('Jnt', 'Ctrl_Offset_Group'), ctrl)
+
+        cmds.parent(fk_01, def_grp)
+        cmds.parent(def_grp, self.asset_names['extras'])
+
+        # IK FK Blending
+        ikfk = self.ctrl_shapes.plus(name=side+"_"+shape+"_IKFK_Switch")
+        ikfk_grp = cmds.group(ikfk, name=ikfk+"_Group")
+        cmds.matchTransform(ikfk_grp, ik_03)
+        attributes = ["tx", "ty", "tz", "rx", "ry", "rz", "sx", "sy", "sz", "v"]
+        cmds.addAttr(
+            ikfk,
+            longName="IKFK",
+            attributeType="double",
+            minValue=0,
+            maxValue=1,
+            defaultValue=0)
+
+        for attr in attributes:
+            cmds.setAttr(f"{ikfk}.{attr}", lock=True, keyable=False, channelBox=False)
 
 
 if __name__ == "__main__":
